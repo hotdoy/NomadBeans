7c103f090e7b7ff6461d3d4ce3821a6f61de1bc9
James <james.auble@gmail.com>
2022-07-09 19:19:37 -0600

    EOD 07092022

--- user/themes/gravel/js/main.js
+++ user/themes/gravel/js/main.js
@@ -5,15 +5,17 @@ window.addEventListener("DOMContentLoaded", (event) => {
 document.addEventListener("alpine:init", () => {
   Alpine.store("gravel", {
     onScrollHandler() {
-      if (window.scrollY > 0) {
+      const siteMain = document.getElementById('site-main')
+      if (siteMain.getBoundingClientRect().top < 64) {
         document.body.classList.add("scrolled")
       } else {
         document.body.classList.remove("scrolled")
       }
     },
     init() {
+      const drawer = document.getElementById('drawer-content')
       window.addEventListener("load", this.onScrollHandler)
-      window.addEventListener("scroll", this.onScrollHandler)
+      drawer.addEventListener("scroll", this.onScrollHandler)
     },
     toggle() {
       this.on = !this.on
