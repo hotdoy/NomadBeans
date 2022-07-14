309f80f351f79ab4593cef671c3caa2801db2f05
James <james.auble@gmail.com>
2022-07-13 23:17:10 -0600

    EOD 07132022

--- user/pages/07.review/review.md
+++ user/pages/07.review/review.md
@@ -1,7 +1,10 @@
 ---
 title: Review
-media_order: 'roman-bozhko-OXXsAafHDeo-unsplash Large.jpeg'
 page_media_alt_desc: 'a menu hung on the wall of a cafe'
+media_order: tyler-nix-2rZq49uGxLk-unsplash.jpeg
+permissions: {  }
+access:
+    site.login: true
 ---
 
 This is the review page
--- user/themes/gravel/js/main.js
+++ user/themes/gravel/js/main.js
@@ -6,16 +6,18 @@ document.addEventListener("alpine:init", () => {
   Alpine.store("gravel", {
     onScrollHandler() {
       const siteMain = document.getElementById('site-main')
-      if (siteMain.getBoundingClientRect().top < 64) {
-        document.body.classList.add("scrolled")
-      } else {
-        document.body.classList.remove("scrolled")
+      if (siteMain) {
+        if (siteMain.getBoundingClientRect().top < 64) {
+          document.body.classList.add("scrolled")
+        } else {
+          document.body.classList.remove("scrolled")
+        }
       }
     },
     init() {
       const drawer = document.getElementById('drawer-content')
       window.addEventListener("load", this.onScrollHandler)
-      drawer.addEventListener("scroll", this.onScrollHandler)
+      if (drawer) drawer.addEventListener("scroll", this.onScrollHandler)
     },
     toggle() {
       this.on = !this.on
